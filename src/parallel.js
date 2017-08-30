19171af61a0629c88cef350fafce258f4f1b4a04
quentin saieb <pitoux16@gmail.com>
2017-08-30 19:56:37 +0200

    Jaspe - parallel function to run async tasks

---
+++ src/parallel.js
@@ -0,0 +1,27 @@
+function parallel(tasks, callback) {
+    let index = 0
+    let completed = 0
+    let results = []
+    let errors = []
+    let length = tasks.length
+
+    if (length === 0) {
+        callback(null)
+    }
+
+    for (; index < length; index++) {
+        tasks[index].call(null, (error, result) => {
+          if (error) {
+            errors.push(error)
+          } else {
+            results.push(result)
+          }
+
+          if ((++completed === length)) {
+            callback(errors, results)
+          }
+        })
+    }
+}
+
+module.exports = parallel
