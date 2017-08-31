4ae3c31ef55b247a88659742130a0da63994ac2f
quentin saieb <pitoux16@gmail.com>
2017-08-31 18:33:26 +0200

    contract export instance of Contract

--- src/contract.js
+++ src/contract.js
@@ -1,34 +1,69 @@
-const Pipeline = require('./pipeline')
 const v = require('./validator')
-const parallel = require('./parallel')
 const Contract = require('./core/contract')
 
 let requirements = new Map()
+let services = new Map()
 
-let username = new Pipeline('AccountContract > CREATE service > username')
-username.add('typeOf string', v.typeOf, {typeOf: 'string'})
-username.add('notNull', v.notNull)
-username.add('notEmpty', v.notEmpty)
-requirements.set('username', username)
-
-let email = new Pipeline('AccountContract > CREATE service > email')
-email.add('typeOf string', v.typeOf, {typeOf: 'string'})
-email.add('notNull', v.notNull)
-email.add('notEmpty', v.notEmpty)
-email.add('email', v.email)
-requirements.set('email', email)
-
-let age = new Pipeline('AccountContract > CREATE service > age')
-age.add('typeOf integer', v.isInteger)
-age.add('notNull', v.notNull)
-age.add('range(0, 110)', v.range, {min: 0, max: 110})
-requirements.set('age', age)
-
-
-module.exports = new Contract( 
-  'AccountContract',
+/**
+ * Create service
+ */
+requirements.set('username', [
   {
-    'create': requirements
+    name: 'typeOf string',
+    validator: v.typeOf,
+    params: {typeOf: 'string'}
+  },
+  {
+    name: 'notNull',
+    validator: v.notNull
+  },
+  {
+    name: 'notEmpty',
+    validator: v.notEmpty
   }
-)
+])
 
+requirements.set('email', [
+  {
+    name: 'typeOf string',
+    validator: v.typeOf,
+    params: {typeOf: 'string'}
+  },
+    {
+    name: 'notNull',
+    validator: v.notNull
+  },
+  {
+    name: 'notEmpty',
+    validator: v.notEmpty
+  },
+  {
+    name: 'email',
+    validator: v.email
+  }
+])
+
+requirements.set('age', [
+  {
+    name: 'typeof integer',
+    validator: v.isInteger
+  },
+  {
+    name: 'notNull',
+    validator: v.notNull
+  },
+  {
+    name: 'range(0, 110)',
+    validator: v.range,
+    params: {min: 0, max: 110}
+  }
+
+])
+services.set('create', requirements)
+
+/**
+ * Delete service
+ */
+
+
+module.exports = new Contract('AccountContract', services)
