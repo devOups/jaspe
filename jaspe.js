9c895a1dba195d9894c97cc7b16aeafe02fba710
quentin saieb <pitoux16@gmail.com>
2017-09-04 15:34:26 +0200

    main file exports jaspe module

---
+++ jaspe.js
@@ -0,0 +1,13 @@
+module.exports = {
+  validator: require('./src/validator'),
+  Contract: require('./src/core/contract'),
+  EntryPoint: require('./src/core/entryPoint'),
+  init: init = (registry) => {
+    let dispatcher = require('./src/core/dispatcher')
+    dispatcher.use(registry)
+    
+    module.exports.invoke = (serviceName, service, params) => {
+      return dispatcher.dispatch(serviceName, service, params)
+    }
+  }
+}
