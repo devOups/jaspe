579e408dbcac21ca4c2cd435813bdfc295d77828
quentin saieb <pitoux16@gmail.com>
2018-02-04 00:25:15 +0100

    parallel: return object instead of array

--- src/parallel.js
+++ src/parallel.js
@@ -9,26 +9,27 @@
 function parallel (tasks, callback) {
   let index = 0
   let completed = 0
-  let results = []
-  let errors = []
-  let length = tasks.length
+  let results = {}
+  const errors = []
+  const length = tasks.length
 
   if (length === 0) {
     callback(null)
   }
 
-  for (; index < length; index++) {
-    tasks[index].call(null, (error, result) => {
+  while (index < length) {
+    tasks[index]((error, result) => {
       if (error) {
         errors.push(error)
       } else {
-        results.push(result)
+        Object.assign(results, result)
       }
 
       if ((++completed === length)) {
         callback(errors, results)
       }
     })
+    ++index
   }
 }
 
